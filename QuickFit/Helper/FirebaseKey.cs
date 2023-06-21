4704c687e630309cb791e03428a0ea6ccf34b23a
Daniel Larkin <64483972+Danielz110@users.noreply.github.com>
2023-06-21 23:17:19 +0100

    Update FirebaseKey.cs

--- QuickFit/Helper/FirebaseKey.cs
+++ QuickFit/Helper/FirebaseKey.cs
@@ -3,8 +3,8 @@ namespace QuickFit.Helper
 {
 	public static class FirebaseKey
 	{
-		public static string apiKey = "AIzaSyCNDNPhX6Y3esD7sQR-cs-so0CJv-p1Qz8";
-		public static string databaseKey = "https://quickfit-2e103-default-rtdb.firebaseio.com/";
+		public static string apiKey = "";
+		public static string databaseKey = "";
 	
 
     }
